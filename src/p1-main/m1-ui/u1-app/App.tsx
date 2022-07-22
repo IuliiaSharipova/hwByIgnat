112a3f9174c158eed567b15e3f9f436b795940d5
Iuliia <sharipova.y.s@gmail.com>
2022-07-22 18:28:44 +0200

    first commit

---
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import './App.module.css'
+import HW1 from '../../../p2-homeworks/h1/HW1'
+
+function App() {
+    return (
+        <div className="Button">
+            <div>react homeworks:</div>
+            <HW1/>
+            {/*<HW2/>*/}
+            {/*<HW3/>*/}
+            {/*<HW4/>*/}
+            {/*<HW5/>*/}
+
+        </div>
+    )
+}
+
+export default App
