1238023d30b95740cd59188f9b0a106a80f07100
Iuliia <sharipova.y.s@gmail.com>
2022-08-26 19:15:15 +0200

    HW2

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import './App.module.css'
 import HW1 from '../../../p2-homeworks/h1/HW1'
+import HW2 from '../../../p2-homeworks/h2/HW2';
 
 function App() {
     return (
         <div className="Button">
             <div>react homeworks:</div>
             <HW1/>
-            {/*<HW2/>*/}
+            <HW2/>
             {/*<HW3/>*/}
             {/*<HW4/>*/}
             {/*<HW5/>*/}
--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {AffairType} from './HW2';
+import s from './Affairs.module.css';
 
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
-
+    const deleteCallback = () => {
+        props.deleteAffairCallback(props.affair._id);
+    };
+    const priorityClass = `${s.text} ${s[props.affair.priority]}`;
     return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
+        <div className={s.div}>
+            <div className={s.text}>{props.affair.name}</div>
+            <div className={priorityClass}>[{props.affair.priority}]</div>
+            <button className={s.deleteButton} onClick={deleteCallback}>X</button>
         </div>
-    )
+    );
 }
 
-export default Affair
+export default Affair;
